8360ab4838feee1e4cad7064dfe3bb86f744872a
Bert Downs <bd65@students.waikato.ac.nz>
2024-07-25 09:15:14 +1200

    fphx doesn't error with modified ceos, but problem still infeasable

--- testing_peng_robinson/solve.py
+++ testing_peng_robinson/solve.py
@@ -14,13 +14,16 @@ m.fs.heater = Heater(property_package=m.fs.props)
  
 # Set inlet conditions for the stream
 m.fs.heater.inlet.flow_mol.fix(10)  # mol/s
-m.fs.heater.inlet.temperature.fix(283)  # K
+#m.fs.heater.inlet.temperature.fix(283)  # K
+m.fs.heater.inlet.enth_mol.fix(2900)  # J/mol
 m.fs.heater.inlet.pressure.fix(100000)  # Pa
 m.fs.heater.inlet.mole_frac_comp[0, "benzene"].fix(0.5)
 m.fs.heater.inlet.mole_frac_comp[0, "toluene"].fix(0.5)
  
 # Set the outlet temperature
-m.fs.heater.outlet.temperature[0].fix(284)  # K
+#m.fs.heater.outlet.temperature[0].fix(284)  # K
+m.fs.heater.outlet.enth_mol[0].fix(2910)  # J/mol
+#m.fs.heater.outlet.enthalpy[0].fix(1000)
  
 # Define the energy balance (heater duty)
 # For some reason, even though this heat duty if calculated by the model,
@@ -35,7 +38,8 @@ results = solver.solve(m, tee=True)
  
 # Display results
 print("\n*** Results ***")
-print("Inlet Temperature (K):", value(m.fs.heater.inlet.temperature[0]))
-print("Outlet Temperature (K):", value(m.fs.heater.outlet.temperature[0]))
+print("Inlet enth_mol (K):", value(m.fs.heater.inlet.enth_mol[0]))
+#print("Inlet Temperature (K):", value(m.fs.heater.inlet.temperature[0]))
+#print("Outlet Temperature (K):", value(m.fs.heater.outlet.temperature[0]))
 print("Heater Duty (J/s):", value(m.fs.heater.heat_duty[0]))
  
