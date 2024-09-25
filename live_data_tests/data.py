792f6d896db3747008b12c7844c492db00c5f850
Bert Downs <bd65@students.waikato.ac.nz>
2024-09-26 10:07:52 +1200

    graphs

--- live_data_tests/data.py
+++ live_data_tests/data.py
@@ -8,7 +8,7 @@ indexes = range(0,20)
 # temperature data: constant at 600 K, with a random noise of 1 K
 temperature = [600 + random.uniform(-1,1) for i in indexes]
 
-# power data: ramp from 100 kw to 1MW
+# power data: ramp from 0 to 20000 W, with a random noise of 3000 W
 power = [(10*i +  random.uniform(-3,3)) * 1000 for i in indexes]
 
 # pressure data: constant at 101325 Pa, with a random noise of 50 Pa
