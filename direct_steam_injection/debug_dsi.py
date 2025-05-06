bdb77164e377c65b155370480d7bcf6fcabc1f05
bd65 <bd65@students.waikato.ac.nz>
2025-05-07 08:50:13 +1200

    reproduce issue in pure IDAES

--- direct_steam_injection/debug_dsi.py
+++ direct_steam_injection/debug_dsi.py
@@ -8,24 +8,35 @@ from idaes.models.properties import iapws95
 from idaes.core.util.initialization import propagate_state
 from idaes.core.util.model_statistics import degrees_of_freedom
 from idaes.core.util import DiagnosticsToolbox
-# need to pip install ahuora_compounds@git+https://github.com/waikato-ahuora-smart-energy-systems/PropertyPackages.git@v0.0.26
-from property_packages.build_package import build_package
+from idaes.models.properties.general_helmholtz import (
+        HelmholtzParameterBlock,
+        HelmholtzThermoExpressions,
+        AmountBasis,
+        PhaseType,
+    )
+from idaes.models.properties.modular_properties import GenericParameterBlock
+from milk_config import milk_configuration
 
 
 m = pyo.ConcreteModel()
 m.fs = FlowsheetBlock(dynamic=False)
-m.fs.steam_properties = build_package("helmholtz", ["water"],["Liq","Vap"])
-m.fs.milk_properties = build_package("milk", ["water","milk_solid"],["Liq","Vap"])
+m.fs.steam_properties = HelmholtzParameterBlock(
+        pure_component="h2o", amount_basis=AmountBasis.MOLE,
+        phase_presentation=PhaseType.LG,
+    )
+m.fs.milk_properties = GenericParameterBlock(**milk_configuration)
 m.fs.dsi = Dsi(property_package=m.fs.milk_properties,steam_property_package=m.fs.steam_properties)
 
 m.fs.dsi.inlet.flow_mol.fix(1)
-m.fs.dsi.properties_milk_in[0].constrain_component(m.fs.dsi.properties_milk_in[0].temperature, 300 * pyo.units.K)
+m.fs.dsi.properties_milk_in[0].temperature.fix( 300 * pyo.units.K)
 m.fs.dsi.inlet.pressure.fix(101325)
-m.fs.dsi.inlet.mole_frac_comp[0,"water"].fix(0.99)
+m.fs.dsi.inlet.mole_frac_comp[0,"h2o"].fix(0.99)
 m.fs.dsi.inlet.mole_frac_comp[0,"milk_solid"].fix(0.01)
 
 m.fs.dsi.steam_inlet.flow_mol.fix(1)
-m.fs.dsi.properties_steam_in[0].constrain_component(m.fs.dsi.properties_steam_in[0].temperature, 300 * pyo.units.K)
+m.fs.dsi.properties_steam_in[0].enth_mol.fix(
+    m.fs.steam_properties.htpx(p=101325 * pyo.units.Pa, T= 300 * pyo.units.K)
+)
 m.fs.dsi.steam_inlet.pressure.fix(101325)
 
 m.fs.dsi.initialize()
@@ -54,8 +65,8 @@ print("Pressure (Pa):", pyo.value(dsi.properties_milk_in[0.0].pressure))
 print("Enthalpy (mol):", pyo.value(dsi.properties_milk_in[0.0].enth_mol))
 print("Enthalpy (mol, Liquid Phase):", pyo.value(dsi.properties_milk_in[0.0].enth_mol_phase["Liq"]))
 print("Enthalpy (mol, Vapor Phase):", pyo.value(dsi.properties_milk_in[0.0].enth_mol_phase["Vap"]))
-print("Mole Fraction (Liquid Phase, Water):", pyo.value(dsi.properties_milk_in[0.0].mole_frac_phase_comp["Liq", "water"]))
-print("Mole Fraction (Vapor Phase, Water):", pyo.value(dsi.properties_milk_in[0.0].mole_frac_phase_comp["Vap", "water"]))
+print("Mole Fraction (Liquid Phase, Water):", pyo.value(dsi.properties_milk_in[0.0].mole_frac_phase_comp["Liq", "h2o"]))
+print("Mole Fraction (Vapor Phase, Water):", pyo.value(dsi.properties_milk_in[0.0].mole_frac_phase_comp["Vap", "h2o"]))
 print("Mole Fraction (Liquid Phase, Milk):", pyo.value(dsi.properties_milk_in[0.0].mole_frac_phase_comp["Liq", "milk_solid"]))
 print("steam_in")
 print("Flow (mol):", pyo.value(dsi.properties_steam_in[0.0].flow_mol))
@@ -64,8 +75,8 @@ print("Pressure (Pa):", pyo.value(dsi.properties_steam_in[0.0].pressure))
 print("Enthalpy (mol):", pyo.value(dsi.properties_steam_in[0.0].enth_mol))
 print("Enthalpy (mol, Liquid Phase):", pyo.value(dsi.properties_steam_in[0.0].enth_mol_phase["Liq"]))
 print("Enthalpy (mol, Vapor Phase):", pyo.value(dsi.properties_steam_in[0.0].enth_mol_phase["Vap"]))
-print("Mole Fraction (Liquid Phase, Water):", pyo.value(dsi.properties_steam_in[0.0].mole_frac_phase_comp["Liq", "water"]))
-print("Mole Fraction (Vapor Phase, Water):", pyo.value(dsi.properties_steam_in[0.0].mole_frac_phase_comp["Vap", "water"]))
+print("Mole Fraction (Liquid Phase, Water):", pyo.value(dsi.properties_steam_in[0.0].mole_frac_phase_comp["Liq", "h2o"]))
+print("Mole Fraction (Vapor Phase, Water):", pyo.value(dsi.properties_steam_in[0.0].mole_frac_phase_comp["Vap", "h2o"]))
 print("steam_cooled")
 print("Flow (mol):", pyo.value(dsi.properties_steam_cooled[0.0].flow_mol))
 print("Temperature (K):", pyo.value(dsi.properties_steam_cooled[0.0].temperature))
@@ -73,8 +84,8 @@ print("Pressure (Pa):", pyo.value(dsi.properties_steam_cooled[0.0].pressure))
 print("Enthalpy (mol):", pyo.value(dsi.properties_steam_cooled[0.0].enth_mol))
 print("Enthalpy (mol, Liquid Phase):", pyo.value(dsi.properties_steam_cooled[0.0].enth_mol_phase["Liq"]))
 print("Enthalpy (mol, Vapor Phase):", pyo.value(dsi.properties_steam_cooled[0.0].enth_mol_phase["Vap"]))
-print("Mole Fraction (Liquid Phase, Water):", pyo.value(dsi.properties_steam_cooled[0.0].mole_frac_phase_comp["Liq", "water"]))
-print("Mole Fraction (Vapor Phase, Water):", pyo.value(dsi.properties_steam_cooled[0.0].mole_frac_phase_comp["Vap", "water"]))
+print("Mole Fraction (Liquid Phase, Water):", pyo.value(dsi.properties_steam_cooled[0.0].mole_frac_phase_comp["Liq", "h2o"]))
+print("Mole Fraction (Vapor Phase, Water):", pyo.value(dsi.properties_steam_cooled[0.0].mole_frac_phase_comp["Vap", "h2o"]))
 print("steam_delta_h")
 print("Delta H:", pyo.value(dsi.steam_delta_h[0.0]))
 print("mixed_unheated")
@@ -84,8 +95,8 @@ print("Pressure (Pa):", pyo.value(dsi.properties_mixed_unheated[0.0].pressure))
 print("Enthalpy (mol):", pyo.value(dsi.properties_mixed_unheated[0.0].enth_mol))
 print("Enthalpy (mol, Liquid Phase):", pyo.value(dsi.properties_mixed_unheated[0.0].enth_mol_phase["Liq"]))
 print("Enthalpy (mol, Vapor Phase):", pyo.value(dsi.properties_mixed_unheated[0.0].enth_mol_phase["Vap"]))
-print("Mole Fraction (Liquid Phase, Water):", pyo.value(dsi.properties_mixed_unheated[0.0].mole_frac_phase_comp["Liq", "water"]))
-print("Mole Fraction (Vapor Phase, Water):", pyo.value(dsi.properties_mixed_unheated[0.0].mole_frac_phase_comp["Vap", "water"]))
+print("Mole Fraction (Liquid Phase, Water):", pyo.value(dsi.properties_mixed_unheated[0.0].mole_frac_phase_comp["Liq", "h2o"]))
+print("Mole Fraction (Vapor Phase, Water):", pyo.value(dsi.properties_mixed_unheated[0.0].mole_frac_phase_comp["Vap", "h2o"]))
 print("Mole Fraction (Liquid Phase, Milk):", pyo.value(dsi.properties_mixed_unheated[0.0].mole_frac_phase_comp["Liq", "milk_solid"]))
 print("output")
 print("Flow (mol):", pyo.value(dsi.properties_out[0.0].flow_mol))
@@ -94,8 +105,8 @@ print("Pressure (Pa):", pyo.value(dsi.properties_out[0.0].pressure))
 print("Enthalpy (mol):", pyo.value(dsi.properties_out[0.0].enth_mol))
 print("Enthalpy (mol, Liquid Phase):", pyo.value(dsi.properties_out[0.0].enth_mol_phase["Liq"]))
 print("Enthalpy (mol, Vapor Phase):", pyo.value(dsi.properties_out[0.0].enth_mol_phase["Vap"]))
-print("Mole Fraction (Liquid Phase, Water):", pyo.value(dsi.properties_out[0.0].mole_frac_phase_comp["Liq", "water"]))
-print("Mole Fraction (Vapor Phase, Water):", pyo.value(dsi.properties_out[0.0].mole_frac_phase_comp["Vap", "water"]))
+print("Mole Fraction (Liquid Phase, Water):", pyo.value(dsi.properties_out[0.0].mole_frac_phase_comp["Liq", "h2o"]))
+print("Mole Fraction (Vapor Phase, Water):", pyo.value(dsi.properties_out[0.0].mole_frac_phase_comp["Vap", "h2o"]))
 print("Mole Fraction (Liquid Phase, Milk):", pyo.value(dsi.properties_out[0.0].mole_frac_phase_comp["Liq", "milk_solid"]))
 
 dt = DiagnosticsToolbox(dsi)
